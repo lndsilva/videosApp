08f48b002f68724011ece119658e093b8632751c
lndsilva <lndsilva@hotmail.com>
2021-05-26 15:31:15 -0300

    Adicionamos o array de filmes e a inferface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,37 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Sem Remorso',
+      lancamento: '30/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos: ['Ação', 'Aventura', 'Thriller', 'Guerra']
+    },
+    {
+      nome: 'Na Mira do Perigo',
+      lancamento: '18/02/2021',
+      duracao: '1h 48m',
+      classificacao: 74,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wAJniojhHa4oVKa9AxdNwvpmS74.jpg',
+      generos: ['Ação', 'Thriller', 'Crime']
+    },
+    {
+      nome: 'Esquadrão Trovão',
+      lancamento: '09/04/2021',
+      duracao: '1h 47m',
+      classificacao: 58,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/Aej7rR73kpAKce6XXBLeqijAwPD.jpg',
+      generos: ['Ação', 'Aventura', 'Comédia']
+    }
+  ];
+
+
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
